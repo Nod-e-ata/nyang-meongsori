3cbf1186bca66029306358614903e87ecf1b2ee3
whitenoise13 <amnesiamar13@gmail.com>
2025-04-21 20:51:09 +0900

    feat: 회원가입 페이지 생성 및 로그인 수정 페이지 아이디 확인 기능 추가

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import React from 'react';
 import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 import './App.css'
 import Login from './pages/login';
+import Signup from './pages/signup';
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Login />}/>
+        <Route path="/login" element={<Login/>}/>
+        <Route path="/signup" element={<Signup/>}/>
       </Routes>
     </Router>
   );
--- src/pages/login.jsx
+++ src/pages/login.jsx
@@ -6,16 +6,28 @@ const Login = () => {
     const [id, setId] = useState('');
     const [password, setPassword] = useState('');
 
-    const handleSubmit = (e) => {
+    const checkIdAvailability = async(id) => {
+        const response = await fetch('/api/check-id?usename=${id}');
+        const data = await response.json();
+        return data.isAvailable;
+    };
+
+    const handleSubmit = async (e) => {
         e.preventDefault();
         console.log('ID:', id);
         console.log('Password:', password);
+
+
+        const isAvailable = await checkIdAvailability(id);
+        if (!isAvailable) {
+            alert("등록되지 않은 아이디입니다.");
+            return;
+        }
     };
 
     return (
         <div className="login-container">
             <img src={logo} alt="로고" className="login-logo"/>
-            {/* <h1 className="login-title"> 냥! 멍소리야 </h1> */}
             <form onSubmit={handleSubmit} className="login-form">
                 <input type="text" placeholder="아이디를 입력해주세요" value={id} onChange={(e) => setId(e.target.value)}/>
                 <input type="password" placeholder="비밀번호를 입력해주세요" value={password} onChange={(e) => setPassword(e.target.value)} />
