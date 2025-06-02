85e56024fd46321df0584fbe1a407c8e48bca523
yeo-won <yw112912@dgsw.hs.kr>
2025-06-02 17:27:50 +0900

    feat: Complete info card and banner
    정보창 및 배너 구현

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,11 +3,33 @@ import './home.css';
 import Topbar from '../components/Topbar';
 
 import plusIcon from '../assets/plus-icon.svg';
+import userIcon from '../assets/user-icon.svg';
+import banner from '../assets/banner.svg';
 
 function Home() {
   return (
     <div className="home-container">
       <Topbar />
+
+      <div className="content-wrapper">
+        <div className="profile-card">
+          <img src={userIcon} alt="User Icon" className="profile-icon"/>
+          <div className="user-name">user</div>
+
+          <div className="info-row">
+            <span className="info-label">반려견 </span>
+            <span className="info-value">1마리</span>
+          </div>
+
+          <div className="info-row">
+            <span className="info-label">반려묘 </span>
+            <span className="info-value">1마리</span>
+          </div>
+        </div>
+
+        <img src={banner} alt="Banner" className="home-banner" />
+      </div>
+
       <button className="floating-button">
         <img src={plusIcon} alt="Plus Icon" className="plus-icon" />
       </button>
